a1f3cafc06c8e6222d1c203972debb017b262b94
Manuel <mramasal@alumnos.unex.es>
2021-02-08 13:18:55 +0100

    arrowImage, list files, list cell files added

---
+++ Tutorial_iOS/viewcontroller/list/ListVC.swift
@@ -0,0 +1,29 @@
+//
+//  ListVC.swift
+//  Tutorial_iOS
+//
+//  Created by Manuel on 08/02/2021.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
