da01f6d8f5dad4fbb5be35b983cea6fa328b99bf
Manuel <mramasal@alumnos.unex.es>
2021-02-10 11:13:16 +0100

    site detail screen design

---
+++ Tutorial_iOS/viewcontroller/detail/SiteDetailVC.swift
@@ -0,0 +1,16 @@
+//
+//  SiteDetailVC.swift
+//  Tutorial_iOS
+//
+//  Created by Manuel on 10/02/2021.
+//
+
+import UIKit
+
+class SiteDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
