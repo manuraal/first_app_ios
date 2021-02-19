7138221a2a573149fe3471335b71a32464804f8b
Manuel <mramasal@alumnos.unex.es>
2021-02-19 12:53:42 +0100

    site detail class

---
+++ Tutorial_iOS/models/SiteDetail.swift
@@ -0,0 +1,19 @@
+//
+//  SiteDetail.swift
+//  Tutorial_iOS
+//
+//  Created by Manuel on 19/02/2021.
+//
+
+import Foundation
+
+class SiteDetail {
+    var id = ""
+    var title = ""
+    var address = ""
+    var transport = ""
+    var email = ""
+    var geocoordinates = ""
+    var description = ""
+    var phone = ""
+}
