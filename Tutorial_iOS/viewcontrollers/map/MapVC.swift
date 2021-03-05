9efb537f61f56d838b7114d4cb00b03bb1e74e82
Manuel <mramasal@alumnos.unex.es>
2021-03-05 10:43:05 +0100

    zoom when opening the map for the first time

--- Tutorial_iOS/viewcontrollers/map/MapVC.swift
+++ Tutorial_iOS/viewcontrollers/map/MapVC.swift
@@ -14,8 +14,17 @@ class MapVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        map.mapType = .standard
-        map.showsUserLocation = true
+        let initialLocation = CLLocation(latitude: 41.3879, longitude: 2.16992)
+        let region = MKCoordinateRegion(
+          center: initialLocation.coordinate,
+          latitudinalMeters: 50000,
+          longitudinalMeters: 60000)
+        map.setCameraBoundary(
+          MKMapView.CameraBoundary(coordinateRegion: region),
+          animated: true)
+        let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+        map.setCameraZoomRange(zoomRange, animated: true)
+
     }
 
 }
